463bf55dda9719fd2e4362df67080516a8df776b
Victoria <victoria@MacBook-Pro-Victoria.local>
2023-10-06 12:02:00 +0300

    Fix Apple generation

--- src/snake/v1/Apple.java
+++ src/snake/v1/Apple.java
@@ -9,10 +9,10 @@ public class Apple {
     public final Point point;
 
     public Apple(GameSize gameSize, List<Point> forbiddenPoints) {
-        int x = rand.nextInt(gameSize.gameWidth) + 1;
-        int y = rand.nextInt(gameSize.gameHeight) + 1;
         Point p;
         do {
+            int x = rand.nextInt(gameSize.gameWidth) + 1;
+            int y = rand.nextInt(gameSize.gameHeight) + 1;
             p = new Point(x, y);
         } while (forbiddenPoints.contains(p)); // TODO: Optimize algorithm
         point = p;
