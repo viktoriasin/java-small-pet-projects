75027ba060867f330b486a4f4a58e03b44c43ab3
Victoria <vicktoria.sin@yandex.ru>
2024-12-16 19:18:10 +0300

    refactor json string parsing algorithm

--- src/json_parser/Main.java
+++ src/json_parser/Main.java
@@ -4,7 +4,10 @@ import json_parser.internal.JsonObjectImpl;
 
 public class Main {
     public static void main(String[] args) {
-        new JsonObjectImpl("{ \"first_name\": \"John\", \"last_name\": \"Smith\", \"is_alive\": true, \"age\": 27}");
+//        new JsonObjectImpl("{ \"first_name\": \"John\", \"last_name\": \"Smith\", \"is_alive\": true, \"age\": 27}");
+        new JsonObjectImpl("{ \"first_name\": \"John\", \"last_name\": \"Smith\", \"is_alive\": true, \"age\": 27, \"array\" : [1, 2,  3], \"number\": 27.3, \"object\" : { \"first_name\": \"John\", \"last_name\": \"Smith\"}}");
+        new JsonObjectImpl("{ \"first_name\": \"John\", \"last_name\": \"Smith\", \"is_alive\": true, \"age\": 27, \"array\" : [{ \"first_name\": \"x\", \"last_name\": \"y\"}, { \"first_name\": \"234!\", \"last_name\": \"'?sdsd\"}], \"number\": 27.3, \"object\" : { \"first_name\": \"John\", \"last_name\": \"Smith\"}}");
+        new JsonObjectImpl("{ \"first_name\": \"John\", \"last_name\": \"Smith\", \"is_alive\": true, \"age\": 27, \"boolean\":true, \"string\": \"Hey!\", \"boolean2\":false}");
     }
 }
 
