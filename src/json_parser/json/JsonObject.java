f2f27171d676610afa94cb8601cb8d681543c0c2
Victoria <vicktoria.sin@yandex.ru>
2024-12-18 15:25:33 +0300

    add annotation handler and field modifiers hanler

--- src/json_parser/json/JsonObject.java
+++ src/json_parser/json/JsonObject.java
@@ -8,5 +8,5 @@ public interface JsonObject {
     // Переводит внутреннее состояние в новый объект `targetType`. В качестве `type` может быть пользовательские класс или record.
     // Generic классы, коллекции или массивы передаваться не будут. Кидает исключение, если json не соответствует типу.
     // Этот метод можно вызывать несколько раз подряд, получая новые объекты
-    <T> T to(Class<T> targetType) throws JsonParserException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
+    <T> T to(Class<T> targetType) throws Exception;
 }
