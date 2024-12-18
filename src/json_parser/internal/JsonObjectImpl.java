db8786658ed12b7ebd71330fb9ffafad650e5e41
Victoria <vicktoria.sin@yandex.ru>
2024-12-18 19:32:53 +0300

    do some refactor

--- src/json_parser/internal/JsonObjectImpl.java
+++ src/json_parser/internal/JsonObjectImpl.java
@@ -1,5 +1,8 @@
 package json_parser.internal;
 
+import json_parser.internal.handlers.AnnotationHandler;
+import json_parser.internal.util.JsonReaderUtil;
+import json_parser.internal.util.JsonToObjectWriterUtil;
 import json_parser.json.JsonObject;
 
 import java.lang.reflect.*;
@@ -9,134 +12,16 @@ public class JsonObjectImpl implements JsonObject {
     public final Map<String, Object> parsedJsonElements;
 
     public JsonObjectImpl(String jsonString) {
-        this.parsedJsonElements = new JsonParserUtil(jsonString).parseObject(); // TODO
-    }
-
-    @SuppressWarnings("unchecked")
-    public Object parse(Class<?> clazz, Object data, Type[] genericTypes) throws Exception {
-        if (clazz == Boolean.class || clazz == String.class
-                || clazz == Integer.class || clazz == Character.class
-                || clazz == Long.class || clazz == Double.class
-                || clazz == Byte.class || clazz == Float.class
-        ) {
-            return data;
-        } else if (clazz.isArray()) {
-            return parseArray(clazz.getComponentType(), (Object[]) data, null); // по условию задания внутри массивов без дженериков
-        } else if (Collection.class.isAssignableFrom(clazz)) {
-            return parseCollection(clazz, (Object[]) data, genericTypes);
-        } else if (Map.class.isAssignableFrom(clazz)) {
-            return parseMap(clazz, (Map<Object, Object>) data, genericTypes);
-        } else if (clazz.isRecord()) {
-            return parseRecord(clazz, (Map<String, Object>) data);
-        } else {
-            return parseObject(clazz, (Map<String, Object>) data);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T> T parseObject(Class<T> clazz, Map<String, Object> data) throws Exception {
-        Field[] fields = clazz.getDeclaredFields();
-        Object instance = clazz.getDeclaredConstructor().newInstance();
-        for (Field field : fields) {
-            String fieldJsonName = AnnotationHandler.resolveFieldName(field);
-            if (data.containsKey(fieldJsonName) && !AnnotationHandler.isFieldIgnored(field)) {
-                Class<?> fieldType = field.getType();
-
-                Object fieldValue = data.get(fieldJsonName);
-                Type genericType = field.getGenericType();
-                Object value;
-                if (genericType instanceof ParameterizedType) {
-                    value = parse(fieldType, fieldValue, ((ParameterizedType) genericType).getActualTypeArguments());
-                } else {
-                    value = parse(fieldType, fieldValue, null);
-                }
-                setObjectField(field, instance, value, clazz);
-            } // TODO добавить обработку случаев, если объекта по имени нет в мапе и ессли поле помечено аннотацией ignored
-        }
-        return (T) instance;
-    }
-
-    public <T> T parseRecord(Class<T> clazz, Map<String, Object> data) throws Exception {
-        RecordComponent[] components = clazz.getRecordComponents();
-        Object[] fieldValues = new Object[components.length];
-
-        for (int i = 0; i < components.length; i++) {
-            RecordComponent comp = components[i];
-            String fieldJsonName = AnnotationHandler.resolveFieldName(comp);
-            String recordComponentName = comp.getName();
-            if (data.containsKey(fieldJsonName) && !AnnotationHandler.isFieldIgnored(comp)) {
-                Class<?> fieldType = comp.getType();
-                Object fieldValue = data.get(fieldJsonName);
-                Object value;
-                if (comp.getGenericType() instanceof ParameterizedType) {
-                    value = parse(fieldType, fieldValue, ((ParameterizedType) comp.getGenericType()).getActualTypeArguments());
-                } else {
-                    value = parse(fieldType, fieldValue, null);
-                }
-                fieldValues[i] = value;
-            }
-        }
-        return (T) canonicalConstructorOfRecord(clazz).newInstance(fieldValues);
-    }
-
-    static <T> Constructor<T> canonicalConstructorOfRecord(Class<T> recordClass)
-            throws NoSuchMethodException, SecurityException {
-        Class<?>[] componentTypes = Arrays.stream(recordClass.getRecordComponents())
-                .map(rc -> rc.getType())
-                .toArray(Class<?>[]::new);
-        return recordClass.getDeclaredConstructor(componentTypes);
-    }
-
-    public Object[] parseArray(Class<?> arrayElementType, Object[] arrayData, ParameterizedType genericType) throws Exception {
-        Object[] extractedObject = (Object[]) Array.newInstance(arrayElementType, arrayData.length);
-        for (int i = 0; i < arrayData.length; i++) {
-            extractedObject[i] = parse(arrayElementType, arrayData[i], null); // по условию задания внутри массивов без дженериков
-        }
-        return extractedObject;
-    }
-
-    public Object parseMap(Class<?> clazz, Map<Object, Object> data, Type[] genericTypes) throws Exception {
-        Map<Object, Object> map = new HashMap<>(); // для упрощения берем базовые реализации. В идеале - надо проверять на конкретный тип
-        for (Map.Entry<Object, Object> o : data.entrySet()) {
-            Object key = parse((Class<?>) genericTypes[0], o.getKey(), null); // по условию задания внутри дженериков без дженериков
-            Object value = parse((Class<?>) genericTypes[1], o.getValue(), null); // по условию задания внутри дженериков без дженериков
-            map.put(key, value);
-        }
-        return map;
-    }
-
-    public Object parseCollection(Class<?> clazz, Object[] data, Type[] genericTypes) throws Exception {
-        Collection<Object> collection;
-        if (List.class.isAssignableFrom(clazz)) {
-            collection = new ArrayList<>(); // для упрощения берем базовые реализации. В идеале - надо проверять на конкретный тип
-        } else if (Set.class.isAssignableFrom(clazz)) {
-            collection = new HashSet<>();
-        } else {
-            collection = new ArrayDeque<>();
-        }
-        for (Object collectionMember : data) {
-            collection.add(parse((Class<?>) genericTypes[0], collectionMember, null)); // по условию задания внутри дженериков без дженериков
-        }
-        return collection;
-    }
-
-    public void setObjectField(Field field, Object instance, Object value, Class<?> clazz) throws Exception {
-        if (!Modifier.isPublic(field.getModifiers())) {
-            Method setterMethod = null;
-            String fieldName = field.getName();
-            setterMethod = clazz.getMethod("set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1), field.getType());
-            setterMethod.invoke(instance, value);
-        } else {
-            field.set(instance, value);
-        }
+        this.parsedJsonElements = new JsonReaderUtil(jsonString).readObject(); // TODO
     }
 
     @Override
     public <T> T to(Class<T> targetType) throws Exception {
+        JsonToObjectWriterUtil jsonToObjectWriterUtil = new JsonToObjectWriterUtil();
         if (targetType.isRecord()) {
-            return parseRecord(targetType, parsedJsonElements);
+            return jsonToObjectWriterUtil.parseRecord(targetType, parsedJsonElements);
         }
-        return parseObject(targetType, parsedJsonElements);
+        return jsonToObjectWriterUtil.parseObject(targetType, parsedJsonElements);
     }
 }
 
--- src/json_parser/internal/JsonParserImpl.java
+++ src/json_parser/internal/JsonParserImpl.java
@@ -1,141 +1,15 @@
 package json_parser.internal;
 
+import json_parser.internal.util.JsonToStringWriterUtil;
 import json_parser.json.JsonObject;
 import json_parser.json.JsonParser;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.RecordComponent;
-import java.util.Collection;
-import java.util.Map;
-
 public class JsonParserImpl implements JsonParser {
     public static String writeToString(Object value) throws Exception {
-        return writeObject(value);
+        return JsonToStringWriterUtil.writeObject(value);
     }
 
-    static JsonObject parse(String json) {
+    private static JsonObject parse(String json) {
         return new JsonObjectImpl(json);
-    }
-
-    private static String write(Object value) throws Exception {
-        if (value != null) {
-            if (value.getClass().isArray()) {
-                return writeArray((Object[]) value);
-            } else if (Collection.class.isAssignableFrom(value.getClass())) {
-                return writeCollection(value);
-            } else if (Map.class.isAssignableFrom(value.getClass())) {
-                return writeMap((Map<Object, Object>) value);
-            } else if (value.getClass() == Boolean.class
-                    || value.getClass() == Integer.class
-                    || value.getClass() == Long.class || value.getClass() == Double.class
-                    || value.getClass() == Byte.class || value.getClass() == Float.class
-            ) {
-                return value.toString();
-            } else if (value.getClass() == String.class || value.getClass() == Character.class) {
-                return writeString(value);
-            } else if (value.getClass().isRecord()) {
-                return writeRecord(value);
-            } else {
-                return writeObject(value);
-            }
-        } else {
-            return writeNull();
-        }
-    }
-
-    private static String writeMap(Map<Object, Object> value) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        processCollectionStringStart(sb, '{');
-        for (Map.Entry<Object, Object> mapEntry : value.entrySet()) {
-            sb.append(write(mapEntry.getKey())).append(":").append(write(mapEntry.getValue())).append(",");
-        }
-        processCollectionStringEnd(sb, '}');
-        return sb.toString();
-    }
-
-    static String writeRecord(Object object) throws Exception {
-        RecordComponent[] components = object.getClass().getRecordComponents();
-        StringBuilder sb = new StringBuilder();
-        processCollectionStringStart(sb, '{');
-        for (var comp : components) {
-            Field field = object.getClass().getDeclaredField(comp.getName());
-            String fieldJsonName = AnnotationHandler.resolveFieldName(field);
-            String name = field.getName();
-            if (!AnnotationHandler.isFieldIgnored(field)) {
-                Object value = comp.getAccessor().invoke(object);
-                sb.append(fieldJsonName).append(":").append(write(value)).append(",");
-            }
-        }
-        processCollectionStringEnd(sb, '}');
-        return sb.toString();
-    }
-
-    static String writeObject(Object object) throws Exception {
-        Field[] fields = object.getClass().getDeclaredFields();
-        StringBuilder sb = new StringBuilder();
-        processCollectionStringStart(sb, '{');
-        for (Field field : fields) {
-            String fieldJsonName = AnnotationHandler.resolveFieldName(field);
-            String name = field.getName();
-            if (!AnnotationHandler.isFieldIgnored(field)) {
-                Object value = getObjectField(field, object);
-                sb.append(fieldJsonName).append(":").append(write(value)).append(",");
-            }
-        }
-        processCollectionStringEnd(sb, '}');
-        return sb.toString();
-    }
-
-    static String writeArray(Object[] array) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        processCollectionStringStart(sb, '[');
-        for (Object object : array) {
-            sb.append(write(object)).append(",");
-        }
-        processCollectionStringEnd(sb, ']');
-        return sb.toString();
-    }
-
-    static String writeCollection(Object collection) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        processCollectionStringStart(sb, '[');
-        Collection<?> collection1 = (Collection<?>) collection;
-        for (Object object : collection1) {
-            sb.append(write(object)).append(",");
-        }
-        processCollectionStringEnd(sb, ']');
-        return sb.toString();
-    }
-
-    static String writeString(Object string) {
-        return "\"" + string + "\"";
-    }
-
-    static String writeNull() {
-        return "null";
-    }
-
-    static void processCollectionStringEnd(StringBuilder sb, char symbolToAddAtTheEnd) {
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append(symbolToAddAtTheEnd);
-    }
-
-    static void processCollectionStringStart(StringBuilder sb, char symbolToAddAtTheStart) {
-        sb.append(symbolToAddAtTheStart);
-    }
-
-    public static Object getObjectField(Field field, Object instance) throws Exception {
-        Object value;
-        if (!Modifier.isPublic(field.getModifiers())) {
-            Method getterMethod = null;
-            String fieldName = field.getName();
-            getterMethod = instance.getClass().getMethod("get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1), field.getType());
-            value = getterMethod.invoke(instance);
-        } else {
-            value = field.get(instance);
-        }
-        return value;
     }
 }
